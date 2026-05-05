d2ea5cc7c7052eaf1e3b0c92e3a9be1562556ca5
raysabdar-rs <raysabdar@gmail.com>
2026-05-04 19:58:00 -0700

    Create remove-nth.py

---
+++ python/remove-nth.py
@@ -0,0 +1,19 @@
+# Remove the n-th element of a linked list from the end
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def removeNthFromEnd(self, head, n):
+        """
+        :type head: Optional[ListNode]
+        :type n: int
+        :rtype: Optional[ListNode]
+        """
+        fast, slow = head, head
+        for _ in range(n): fast = fast.next
+        if not fast: return head.next
+        while fast.next: fast, slow = fast.next, slow.next
+        slow.next = slow.next.next
+        return head       
